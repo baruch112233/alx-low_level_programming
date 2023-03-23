d8f4d6d410f011c4c4d5571db6cc2a3b9e42a800
baruch112233 <bezalelhareg@gmail.com>
2023-03-23 15:53:18 -0700

    digit printing fixed

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,10 +4,8 @@
  * @x: The number to be checked
  * Return: 1 for a character that will be a diget or 0 for anything else
  */
-
 int _isdigit(int x)
 {
-	
 	if (x >= 48 && x <= 57)
 	{
 		return (1);
