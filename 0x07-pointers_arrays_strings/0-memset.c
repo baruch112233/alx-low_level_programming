078c0b969f6433ec65fd6415644891721b159154
baruch112233 <bezalelhareg@gmail.com>
2023-04-03 07:36:32 -0700

    memset fixed final

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,17 +2,17 @@
 /**
  * _memset - fills memory with constant byte
  * @s: pointer to be modified
- * @b: a constant that modifies s
+ * @b: the constant that replaces the first n bytes of the pointer
  * @n: unsigned int
- * Return: pointer to the memory area s
+ * Return: modified array
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	for(i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
-	return(s);
+	return (s);
 }
