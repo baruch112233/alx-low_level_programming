662063123456e66c51b02df6dfd99a436ed884d0
baruch112233 <bezalelhareg@gmail.com>
2023-04-17 12:01:47 -0700

    fileagain

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - prints name of the file
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
 	printf("%s\n", _FILE_);
 	return (0);
